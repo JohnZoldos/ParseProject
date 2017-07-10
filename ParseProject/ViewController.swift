6132738310209c7cbdaca13f4eb81501272ef8c8
johnzoldos <john.a.zoldos@gmail.com>
2017-07-09 22:42:42 -0400

    Initial Commit

---
+++ ParseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseProject
+//
+//  Created by John Zoldos on 7/9/17.
+//  Copyright © 2017 John Zoldos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
